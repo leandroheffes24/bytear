ad0b638ca0707440d81abb985ec63ba57e52ba5a
leandroheffes24 <leandroheffes@outlook.es>
2024-10-08 09:13:51 -0300

    home banner correction complete

--- src/app/components/homeBannerSection/homeBannerSection.tsx
+++ src/app/components/homeBannerSection/homeBannerSection.tsx
@@ -5,14 +5,28 @@ import HomeMobileBanner1 from '../../../../public/img/banner1mobile.png'
 import HomeMobileBanner2 from '../../../../public/img/banner2mobile.jpg'
 import HomeDesktopBanner1 from '../../../../public/img/banner1Desktop.png'
 import HomeDesktopBanner2 from '../../../../public/img/banner2Desktop.jpg'
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
+import { useEffect, useState } from 'react';
 
 export default function HomeBannerSection(){
+    const [banner1, setBanner1] = useState<StaticImageData | string>('')
+    const [banner2, setBanner2] = useState<StaticImageData | string>('')
+
+    useEffect(() => {
+        if(screen.width < 992){
+            setBanner1(HomeMobileBanner1)
+            setBanner2(HomeMobileBanner2)
+        } else {
+            setBanner1(HomeDesktopBanner1)
+            setBanner2(HomeDesktopBanner2)
+        }
+    }, [])
+
     return(
         <Splide
             options={{
                 height: '290px',
-                lazyLoad: 'sequential',
+                lazyLoad: 'nearby',
                 breakpoints: {
                     767:{
                         height: '250px'
@@ -28,18 +42,14 @@ export default function HomeBannerSection(){
         >
             <SplideSlide className={styles.splideSlide}>
                 <Image
-                    data-splide-lazy-srcset={`${HomeMobileBanner1} 991w, ${HomeDesktopBanner1} 1199w`}
-                    data-splide-lazy={HomeDesktopBanner1}
-                    src={HomeDesktopBanner1}
+                    src={banner1}
                     alt='Banner Inicio 1'
                     fill
                 />
             </SplideSlide>
             <SplideSlide className={styles.splideSlide}>
                 <Image
-                    data-splide-lazy-srcset={`${HomeMobileBanner2} 991w, ${HomeDesktopBanner2} 1199w`}
-                    data-splide-lazy={HomeDesktopBanner2}
-                    src={HomeDesktopBanner2}
+                    src={banner2}
                     alt='Banner Inicio 2'
                     fill
                 />
